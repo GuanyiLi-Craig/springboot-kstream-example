5767a1a5420784f0d3efe473fdfdd23fbc8892a2
GuanyiLi-Craig <BitForce.Studio@gmail.com>
2022-04-23 22:02:13 +0100

    Init commit

---
+++ src/main/java/com/bitforce/data/kstreamexp/KstreamexpApplication.java
@@ -0,0 +1,13 @@
+package com.bitforce.data.kstreamexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KstreamexpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KstreamexpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitforce/data/kstreamexp/KstreamexpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitforce.data.kstreamexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KstreamexpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
